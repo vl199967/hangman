db07fb97bd1009f38ccadecc4dc54e977c211e96
vl199967 <vl199967@gmail.com>
2022-11-05 12:17:06 +0000

    This commit completes milestone 3 task 1.

---
+++ milestone_3.py
@@ -0,0 +1,9 @@
+while True:
+    guess = input("guess one letter!")
+    if len(guess) == 1:
+        break 
+    else:
+        print("Invalid letter. Please, enter a single alphabectical character.")
+
+
+        
