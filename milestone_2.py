47b9c7d9fc41f0337f4f48d8592a29ec2ff3ee32
vl199967 <vl199967@gmail.com>
2022-11-05 11:55:58 +0000

    this commit imports the random library, randomly chooses a word from word list, and asks user for an input. completes task 3

--- milestone_2.py
+++ milestone_2.py
@@ -1,3 +1,11 @@
+import random 
+
 word_list = ['mango','custard apple','lychee','orange','banana']
 
 print(word_list)
+
+word = random.choice(word_list)
+
+print(word)
+
+guess = input("enter a radnom single letter")
