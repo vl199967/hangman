08af33578bc6d26141f78a65f8161dcc944e9e87
vl199967 <vl199967@gmail.com>
2022-11-05 11:59:29 +0000

    this commit adds a placeholder input validation and hence completes task 4.

--- milestone_2.py
+++ milestone_2.py
@@ -9,3 +9,8 @@ word = random.choice(word_list)
 print(word)
 
 guess = input("enter a radnom single letter")
+
+if len(guess) == 1:
+    print("good guess!")
+else: 
+    print("Oops! That is not a valid input.")    
