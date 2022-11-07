5d6fc6cf2b86cca166f4f750c3783b7147b6a6f8
vl199967 <vl199967@gmail.com>
2022-11-07 15:49:24 +0000

    This commit refined validation, hence completing milestone 3 task 3.

--- milestone_3.py
+++ milestone_3.py
@@ -1,11 +1,22 @@
 from milestone_2 import word as wrd 
 
-
+'''
 while True:
     guess = input("guess one letter!")
     if len(guess) == 1 & (guess in wrd):
-        print("good guess mate")
+        print("Good guess! {} is in the word.".format(guess))
         break 
     else:
-        print("Invalid letter. Please, enter a single alphabectical character.")    
+        print("Not singular character input or guessed letter is not in the word. Try again.")    
+'''
+def check_guess():
+    while True:
+        guess = input("guess one letter!")
+        if len(guess) == 1 & (guess in wrd):
+            print("Good guess! {} is in the word.".format(guess))
+            break 
+        else:
+            print("Not singular character input or guessed letter is not in the word. Try again.")   
 
+check_guess()            
+    
