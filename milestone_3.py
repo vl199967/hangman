e8c56c28e6663489d7f241410b1130e5677a88f3
vl199967 <vl199967@gmail.com>
2022-11-07 14:46:37 +0000

    In this commit, the placeholder validation in milestone_2.py gets turned into comment. The validation logic in milestone_3.py gets updated but is not yet perfect.

--- milestone_3.py
+++ milestone_3.py
@@ -1,7 +1,11 @@
+from milestone_2 import word as wrd 
+
+
 while True:
     guess = input("guess one letter!")
-    if len(guess) == 1:
+    if len(guess) == 1 & (guess in wrd):
+        print("good guess mate")
         break 
     else:
-        print("Invalid letter. Please, enter a single alphabectical character.")
+        print("Invalid letter. Please, enter a single alphabectical character.")    
 
